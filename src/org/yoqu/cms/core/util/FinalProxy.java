295b26c3d37affbedbfc852290ca227bec0125ff
yoqu <yoqulin@gmail.com>
2016-04-25 10:23:50 +0800

    更新hook实现方式，使用框架自带的AOP工具

--- src/org/yoqu/cms/core/util/FinalProxy.java
+++ src/org/yoqu/cms/core/util/FinalProxy.java
@@ -1,10 +1,11 @@
 package org.yoqu.cms.core.util;
 
+import com.jfinal.aop.Enhancer;
+import com.jfinal.aop.Interceptor;
+import com.jfinal.aop.Invocation;
+import com.jfinal.core.Controller;
 import com.jfinal.kit.LogKit;
 import com.jfinal.kit.StrKit;
-import net.sf.cglib.proxy.Enhancer;
-import net.sf.cglib.proxy.MethodInterceptor;
-import net.sf.cglib.proxy.MethodProxy;
 import org.yoqu.cms.core.admin.config.Hook;
 
 import java.lang.reflect.InvocationTargetException;
@@ -14,16 +15,18 @@ import java.util.Set;
 /**
  * 通过Cglib实现在方法调用前后向控制台输出两句字符串
  */
-public class FinalProxy implements MethodInterceptor {
+public class FinalProxy implements Interceptor {
 
-    //要代理的原始对象  
-    private Object obj;
     //要执行的hook
     private String hookName;
     //要被执行的方法.
     private Set<Class<?>> deals = null;
 
+    //被回调的方法名字
     private String invokeMethodName = null;
+
+    //回调的参数..
+    private Object[] args=null;
 
     /**
      * 创建代理
@@ -43,7 +46,7 @@ public class FinalProxy implements MethodInterceptor {
             e.printStackTrace();
         }
         this.hookName = hookName;
-        return Enhancer.create(target, this);
+        return Enhancer.enhance(target, this);
     }
 
     /**
@@ -66,35 +69,14 @@ public class FinalProxy implements MethodInterceptor {
         return calssList;
     }
 
-    /**
-     * 回调方法:在代理实例上拦截并处理目标方法的调用，返回结果
-     *
-     * @param proxy       代理类
-     * @param method      被代理的方法
-     * @param params      该方法的参数数组
-     * @param methodProxy
-     */
-    @Override
-    public Object intercept(Object proxy, Method method, Object[] params, MethodProxy methodProxy) throws Throwable {
-        //如果没有扫描到注入的类直接执行回调函数.
-        if (deals == null) {
-            return methodProxy.invokeSuper(proxy, params);
-        }
-        // 调用之前
-        // 调用目标方法，用methodProxy,
-        // 而不是原始的method，以提高性能
-        if (method.getName().equals(invokeMethodName)) {
-            doBefore();
-        }
-        return methodProxy.invokeSuper(proxy, params);
-    }
 
     private void doBefore() {
         for (Class one : deals) {
             try {
                 Method method = one.getMethod(StrKit.firstCharToLowerCase(one.getSimpleName()) + hookName, null);
                 Object object = com.jfinal.aop.Enhancer.enhance(one);
-                LogKit.info("Method Hook     : " + StrKit.firstCharToLowerCase(one.getSimpleName()) + hookName);
+                LogKit.info("Method Hook     : " + one.getName()+" > "+StrKit.firstCharToLowerCase(one.getSimpleName()) + hookName);
+
                 method.invoke(object);
             } catch (NoSuchMethodException e) {
                 e.printStackTrace();
@@ -106,9 +88,17 @@ public class FinalProxy implements MethodInterceptor {
         }
     }
 
-    private void doAfter() {
-//        for (InvokeDeal invokeDeal : deals) {
-//            invokeDeal.afterDeal();
-//        }
+    @Override
+    public void intercept(Invocation inv) {
+        //如果没有扫描到注入的类直接执行回调函数.
+        if (deals == null) {
+            inv.invoke();
+        }
+
+        if (inv.getMethod().getName().equals(invokeMethodName) && inv.getMethod().getParameterCount()==args.length) {
+            doBefore();
+            Controller controller = (Controller) inv.getArg(0);
+        }
+        inv.invoke();
     }
 }
